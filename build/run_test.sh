801cb10d9c5d240f673a6ed89dc9e48c68ed95d8
laico-vaio3 <laico-vaio3@laico-vaio3.(none)>
2011-06-09 11:58:47 -0300

    Adicionando um arquivo .sh para facilitar os testes

---
+++ build/run_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+clear
+
+echo "Configurando variavel de ambiente"
+export PYTHONPATH=/home/laico-vaio3/workspace/coruja/hardware/serial_test_adapter/bin
+echo "Inicio do programa"
+echo ""
+
+./../bin/hadapter
